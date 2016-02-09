1272984457a092a89fee30679a7f13adbd8db7c5
Neeraj Prashar <nprashar@ntdev.microsoft.com>
2016-02-08 16:05:22 -0800

    Adding the eventracing file

---
+++ EventTracing/EventTracing/Events.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Diagnostics.Tracing;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourceTransferEventEx
+{
+    public class SimpleData
+    {
+        public string Name { get; set; }
+        public int Address { get; set; }
+    }
+    public class NodeData
+    {
+        public string NodeName { get; set; }
+        public int NodeId { get; set; }
+        public int[] ChildNodeIds { get; set; }
+        public Dictionary<string, int> UserValues { get; set; }
+    }
+
+    [EventSource(Name = "SampleEvents-Demo")]
+    public sealed class Events : EventSource
+    {
+        public static Events Log = new Events();
+
+        //[Event(1,Level=EventLevel.Informational,Message="Transfer,ReqID={1},Title={2}")]
+        public void TransferEvent(Guid relatedId, int reqId, string title)
+        {
+            WriteEventWithRelatedActivityId(1, relatedId, reqId, title);
+        }
+
+        //[Event(2, Message = "Simple,ReqID={0},Title={1}")]
+        public void SimpleEvent(int reqId, string title)
+        {
+            WriteEvent(2, reqId, title);
+        }
+
+        //[Event(3,Level=EventLevel.Informational,Message="")]
+        public void LogNode(string message, NodeData data)
+        {
+            WriteEvent(3, message, data);
+        }
+
+        public void LogDictionary(string message, Dictionary<string, int> keyValues)
+        {
+            WriteEvent(4, message, keyValues);
+        }
+
+        public void LogSimpleData(string message, string data)
+        {
+            WriteEvent(5, message, data);
+        }
+
+        public void LogArray(string message, IEnumerable<int> data)
+        {
+            WriteEvent(6, message, data);
+        }
+    }
+
+    public class MyEventListener : EventListener
+    {
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            var id = eventData.ActivityId;
+            var rid = eventData.RelatedActivityId;
+            var rName = eventData.EventName;
+            Console.WriteLine("ActivityId: {0}, RelatedActivityId :{1}", id, rid);
+        }
+    }
+}
---
+++ EventTracing/EventTracing/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourceTransferEventEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var listener = new MyEventListener())
+            {
+                var relatedActivityId = Guid.Parse("abb50c46-c0b5-4cfc-b1f0-f5d19d3501d1");
+                var activityId = Guid.Parse("becfb032-78e6-44e1-91e9-025e4f2c9ba0");
+
+                MyEventSource.SetCurrentThreadActivityId(activityId);
+
+                listener.EnableEvents(MyEventSource.Log, EventLevel.LogAlways);
+
+                // keeps activity Id in EventArgs; relatedId = Guid.Empty
+                MyEventSource.Log.EventEx(1, "test");
+
+                // passes both ActivityId and RelatedActivityId to EventListener callback
+                MyEventSource.Log.TransferEventEx(relatedActivityId, 1, "test");
+
+                var aList = new List<int> { 3, 4, 5, 6 };
+                var aDictionary = new Dictionary<string, int>() { { "user1", 1 }, { "user2", 2 } };
+                var aNode = new NodeData
+                {
+                    NodeName = "Test",
+                    NodeID = 1,
+                    ChildNodeIds = new int[] { 3, 4, 5 },
+                    UserValues = aDictionary
+                };
+                MyEventSource.Log.LogArray("testMessage", aList);
+
+            }
+        }
+    }
+    /*
+    public sealed class MyEventSource : EventSource
+    {
+        public static MyEventSource Log = new MyEventSource();
+
+        [Event(1, Message="ReqId: {0}, Title:{1}")]
+        public void TransferEventEx(Guid relatedActivityId, int reqId, string title)
+        {
+            WriteEventWithRelatedActivityId(1, relatedActivityId, reqId, title);
+        }
+
+        [Event(2, Message = "Transfer Event")]
+        public void EventEx(int reqId, string title)
+        {
+            WriteEvent(2, reqId, title);
+        }
+    }
+
+    public class MyEventListener : EventListener
+    {
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            //throw new NotImplementedException();
+            var activityId = eventData.ActivityId;
+            var relatedActivityId = eventData.RelatedActivityId;
+
+            Console.WriteLine("ActivityId: {0}, RelatedActivityId :{1}", activityId, relatedActivityId);
+        }
+    }
+     * */
+}
